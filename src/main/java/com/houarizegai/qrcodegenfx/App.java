3be711d9dc19c28a5c2ea394e1692bf64dcab0b7
HouariZegai <houarizegai14@gmail.com>
2019-12-02 20:20:52 +0100

    Making QRCode front-end (fxml+css)

---
+++ src/main/java/com/houarizegai/qrcodegenfx/App.java
@@ -0,0 +1,29 @@
+package com.houarizegai.qrcodegenfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(""));
+
+            stage.setScene(new Scene(root));
+            stage.setTitle("QR Code Generator");
+            stage.show();
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/houarizegai/qrcodegenfx/controllers/QRCodeGenController.java
@@ -0,0 +1,13 @@
+package com.houarizegai.qrcodegenfx.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class QRCodeGenController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
